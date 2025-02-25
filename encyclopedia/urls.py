d31d8a3a7199adbcbe2c3e3f397aa7a949ed1ee1
safefy-Dev <safe.th@hotmail.com>
2025-02-25 09:41:21 +0700

    05

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
     path("new/",views.new_page, name="new_page"),
-    path("wiki/edit/<str:title>", views.edit_page, name="edit_page"),  # Edit Page Route
+    path("edit/<str:title>", views.edit_page, name="edit_page"),  # Edit Page Route
     path("random/", views.random_page, name="random_page"),  # Random Page Route
 ]
 
